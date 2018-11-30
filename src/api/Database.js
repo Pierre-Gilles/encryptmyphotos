f540d5214dbd4eb44281a7f83e93d2ebd60521e9
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-11-30 12:02:47 +0800

    Use more recent blockstack library

--- src/api/Database.js
+++ src/api/Database.js
@@ -6,28 +6,39 @@ let files = [];
 
 const Database = {
   getImageList: async () => {
-    let filesString = await blockstack.getFile(DATABASE_FILENAME);
+    let filesString = await blockstack.getFile(DATABASE_FILENAME, {
+      decrypt: true
+    });
     if (filesString) {
       files = JSON.parse(filesString);
     }
+
     return files;
   },
   loadImages: async (files) => {
     let promises = [];
     files.forEach((file) => {
-      promises.push(blockstack.getFile(file.id));
+      promises.push(blockstack.getFile(file.id, {
+        decrypt: true
+      }));
     });
     return Promise.all(promises);
   },
   initAccount: () => {
-    return blockstack.putFile(DATABASE_FILENAME, JSON.stringify([]));
+    return blockstack.putFile(DATABASE_FILENAME, JSON.stringify([], {
+      encrypt: true
+    }));
   },
   saveDatabase: () => {
-    return blockstack.putFile(DATABASE_FILENAME, JSON.stringify(files));
+    return blockstack.putFile(DATABASE_FILENAME, JSON.stringify(files), {
+      encrypt: true
+    });
   },
   uploadImage: async (file, content) => {
     file.id = uuid.v4();
-    await blockstack.putFile(file.id, content);
+    await blockstack.putFile(file.id, content, {
+      encrypt: true
+    });
     files.push(file);
     await Database.saveDatabase();
     return content;
